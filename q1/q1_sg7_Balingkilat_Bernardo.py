9d96b614fb2b44b432ba5b8f85c338616557ecff
gardenofaeden <ambernardo2030@clc.pshs.edu.ph>
2026-09-24 06:49:50 +0800

    Implement Glassware, Beaker, and Tray classes
    Added classes for Glassware, Beaker, and Tray with initialization and deletion methods.

--- q1/q1_sg7_Balingkilat_Bernardo.py
+++ q1/q1_sg7_Balingkilat_Bernardo.py
@@ -1,1 +1,27 @@
+class Glassware:
+    def __init__(self,name='glassware'):
+        self.name = name
+        print(f"{self.name} has been created.")
+    def functions(self):
+        print(f"This {self.name} holds nothing.")
+        
+class Beaker (Glassware):
+    def __init__(self,name="Beaker"):
+        super().__init__(name) 
+    def __del__(self):
+        print(f"{self.name} is gone") 
 
+class Tray:
+    def __init__(self):
+        self.glassware = Glassware("Glassware") 
+        self.beaker = [Beaker(f"Beaker{i+1}") for i in range(5)]
+        print("\nThe tray has been created.")
+    def glassexist(self):
+        print("Glassware is still existing")
+    def _del_(self):
+        del self.beaker
+        print("\nTray has been deleted")
+        
+labGlass = Tray()
+labGlass.glassexist()
+del labGlass
